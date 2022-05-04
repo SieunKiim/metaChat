8244594e6a388dc9ba59526e2fc68be500eb7adb
gimsieun <sieun960513@naver.com>
2022-05-04 20:15:24 +0900

    pub sub 구조 다시

--- src/main/java/com/sieun/metaChat/WebSocketConfig.java
+++ src/main/java/com/sieun/metaChat/WebSocketConfig.java
@@ -1,7 +1,5 @@
 package com.sieun.metaChat;
 
-import com.sieun.metaChat.handler.WebSocketHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -14,23 +12,14 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-//    private final WebSocketHandler chatHandler;
-//
-//    // 도메인이 다른 서버에서도 원활히 데이터를 주고받을 수 있도록 .setAllowedOrighins("*") -> 전부 다 받는 것으로 설정
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) { // 웹 소켓 핸드쉐이크
-//        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*"); // 웹 소켓에 연결할 주소 (ws://~~~/chat)
-//    }
-
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/example").setAllowedOrigins("*");
+        registry.addEndpoint("/university").setAllowedOrigins("*"); // websocket 핸드셰이크를 위한 endpoint 지정
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/pub");
+        config.setApplicationDestinationPrefixes("/pub"); // 대상 헤더가 시작되는 STOMP 메시지는 해당 클래스의 메서드로 라우팅됨
         config.enableSimpleBroker("/sub");
     }
 }
--- src/main/java/com/sieun/metaChat/controller/StompChatController.java
+++ src/main/java/com/sieun/metaChat/controller/StompChatController.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Controller;
 public class StompChatController {
     private final SimpMessagingTemplate template; // 특정 Broker로 메시지를 전달
 
-    @MessageMapping(value = "/chat/enter")
+    @MessageMapping("/chat/enter")
     public void enter(ChatMessageDTO message) {
         message.setMessage(message.getWriter() + "님이 채팅방에 입장하였습니다.");
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
-    @MessageMapping(value = "/chat/message")
+    @MessageMapping("/chat/message")
     public void message(ChatMessageDTO message) {
         template.convertAndSend("sub/chat/room/" + message.getRoomId(), message);
     }
